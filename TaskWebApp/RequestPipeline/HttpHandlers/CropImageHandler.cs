6327fa52814ae48e48ea65f0805162b795f0b1ec
KseniaLa <ksla1998@mail.ru>
2019-05-26 19:31:08 +0300

    set up project

---
+++ TaskWebApp/RequestPipeline/HttpHandlers/CropImageHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+
+namespace RequestPipeline.HttpHandlers
+{
+     public class CropImageHandler : IHttpHandler
+     {
+          public bool IsReusable
+          {
+               get { return false; }
+          }
+
+          public void ProcessRequest(HttpContext context)
+          {
+               var imagePath = $"{AppDomain.CurrentDomain.BaseDirectory}{context.Request.FilePath}";
+
+               if (!File.Exists(imagePath))
+               {
+                    context.Response.StatusCode = 400;
+                    context.Response.Write("File not found");
+                    context.Response.End();
+                    return;
+               }
+
+               var img = new Bitmap(imagePath);
+
+               var requestUrl = new Uri(HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Authority + HttpContext.Current.Request.RawUrl);
+
+               var sizeParameter = HttpUtility.ParseQueryString(requestUrl.Query).Get("size");
+
+               var temp = new MemoryStream();
+
+               if (!string.IsNullOrEmpty(sizeParameter))
+               {
+                    var size = int.Parse(sizeParameter);
+                    img = CropImage(img, new Rectangle(0, 0, size, size));
+               }
+
+               img.Save(temp, ImageFormat.Jpeg);
+               var buffer = temp.GetBuffer();
+               context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+               context.Response.End();
+          }
+
+          public Bitmap CropImage(Bitmap source, Rectangle section)
+          {
+               var bmp = new Bitmap(section.Width, section.Height);
+
+               var g = Graphics.FromImage(bmp);
+
+               g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+
+               return bmp;
+          }
+     }
+}
---
+++ TaskWebApp/RequestPipeline/HttpModules/RequestHeadersModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web;
+
+namespace RequestPipeline.HttpModules
+{
+     public class RequestHeadersModule : IHttpModule
+     {
+          public void Dispose()
+          {
+
+          }
+
+          public void Init(HttpApplication context)
+          {
+               context.BeginRequest += OnBegin;
+               context.EndRequest += OnEnd;
+          }
+
+          public void OnBegin(Object source, EventArgs e)
+          {
+               var context = ((HttpApplication)source).Context;
+
+               context.Response.AddHeader("Start Time", DateTime.Now.ToString("O"));
+               context.Response.AddHeader("IP address", context.Request.UserHostAddress ?? "undefined");
+          }
+
+          public void OnEnd(Object source, EventArgs e)
+          {
+               var context = ((HttpApplication)source).Context;
+
+               context.Response.AddHeader("End Time", DateTime.Now.ToString("O"));
+          }
+
+     }
+}
